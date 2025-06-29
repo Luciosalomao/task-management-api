5f860bdac70835351209ea3c172ef0bdb2ec8b8c
Lucio Marcos Salomão de Souza <lucio.souza@el.com.br>
2025-06-29 19:37:03 -0300

    Validação da dto task

--- src/task/task.dto.ts
+++ src/task/task.dto.ts
@@ -1,8 +1,39 @@
+import {
+  IsUUID,
+  MaxLength,
+  MinLength,
+  IsString,
+  IsEnum,
+  IsOptional,
+  IsDateString,
+} from 'class-validator';
+
+export enum TaskStatusEnum {
+  TO_DO = 'TO_DO',
+  IN_PROGRESS = 'IN_PROGRESS',
+  DONE = 'DONE',
+}
+
 export class TaskDto {
+  @IsUUID()
+  @IsOptional()
   id: string;
+
+  @IsString()
+  @MinLength(3)
+  @MaxLength(256)
   title: string;
+
+  @IsString()
+  @MinLength(5)
+  @MaxLength(512)
   description: string;
+
+  @IsEnum(TaskStatusEnum)
+  @IsOptional()
   status: string;
+
+  @IsDateString()
   expirationDate: Date;
 }
 
