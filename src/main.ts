3e29d0937f934abc8b619a720f31d155c1a5a753
Lucio Marcos Salomão de Souza <lucio.souza@el.com.br>
2025-07-06 21:03:36 -0300

    Habilitando Cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,15 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(new ValidationPipe());
+
+  //Habilitando cor na porta 4000
+  app.enableCors({
+    origin: 'http://localhost:4000',
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
